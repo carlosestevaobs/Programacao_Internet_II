a0150fa2873e612d1711c05a7f525d3e6e628339
Carlos Estevão <39769751+carlosestevaobs@users.noreply.github.com>
2023-11-20 14:27:16 -0300

    Cap. 7
    Lendo arquivo JSON

---
+++ reactjs/src/Componentes/Table/index.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import ContainerForm from "../ContainerForm";
+
+const Tb = styled.table`
+  border-collapse: collapse;
+`;
+
+const Tr = styled.tr`
+  background-color: #eeeff1;
+
+  &:nth-child(even) {
+    background-color: #0e6e1e55;
+  }
+`;
+
+const Th = styled.th`
+  background-color: #00420c;
+  font-weight: lighter;
+  color: #fff;
+  padding: 6px 24px;
+`;
+
+const Td = styled.td`
+  padding: 6px 24px;
+`;
+
+function Table({ cabecalho, registros }) {
+  const renderizarLista = (dados) => {
+    return Object.values(dados).map((dado) => <Td>{dado}</Td>);
+  };
+
+  return (
+    <ContainerForm>
+      <Tb>
+        <thead>
+          <Tr>
+            {cabecalho.map((cab, index) => (
+              <Th key={index}>{cab}</Th>
+            ))}
+          </Tr>
+        </thead>
+        <tbody>
+          {registros.map((registro, ind) => (
+            <Tr key={ind}>{renderizarLista(registro)}</Tr>
+          ))}
+        </tbody>
+      </Tb>
+    </ContainerForm>
+  );
+}
+
+export default Table;
--- reactjs/src/Paginas/Cursos/index.jsx
+++ reactjs/src/Paginas/Cursos/index.jsx
@@ -1,13 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import ContainerPagina from "../../Componentes/ContainerPagina";
 import { NameContext } from "../../Contextos/NameContext";
+import Table from "../../Componentes/Table"
 
 function Cursos() {
     const { nome } = useContext(NameContext);
+    const cabecalho = ["ID", "Cursos", "Quantidade de alunos"];
+    const [cursos, setCursos] = useState([]);
+
+    useEffect(() => {
+        const lerArquivoJson = async () => {
+            try {
+                const data = require("./cursos.json");
+                setCursos(data);
+            } catch (error) {
+                console.error("Erro ao ler o arquivo JSON:", error);
+            }
+        };
+
+        lerArquivoJson();
+    }, []);
 
     return (
         <ContainerPagina>
             <b>{nome},</b> esta é a página de cursos.
+            <Table cabecalho={cabecalho} registros={cursos} />
         </ContainerPagina>
     );
 }
